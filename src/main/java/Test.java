b43a6577ccbaec9674423face6985c381d7166db
chpawan <chpawankr@gmail.com>
2019-07-10 15:26:36 +0530

    working fixed

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,14 +7,13 @@ public class Test {
 		Scanner sc=new Scanner(System.in);
 		double op1=sc.nextDouble();
 		double op2 = sc.nextDouble();
-		String operator = sc.nextLine();
+		String operator = sc.next();
 		
 		double result=0;
 		Addition add=new Addition();
 		Subtraction sub= new Subtraction();
 		Multiplication mult =new Multiplication();
 		Division div =new Division();
-		
 		if(operator.equals("+")) 
 			result =add.evaluate(op1,op2);
 		else if(operator.equals("-")) 
