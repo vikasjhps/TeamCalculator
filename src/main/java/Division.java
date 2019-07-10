f5343f52b58e4d7c5160d534487ac5e2cb048e3e
Vikas Kumar <vikas.jhps@gmail.com>
2019-07-10 14:18:27 +0530

    test

--- src/main/java/Division.java
+++ src/main/java/Division.java
@@ -2,15 +2,15 @@
 public class Division implements ArithmeticOperations{
 
 	public double evaluate(double op1, double op2) {
-		double result = 0;
+		double solution = 0;
 		try {
-			result = op1/op2;
+			solution = op1/op2;
 		}
 		catch(Exception e){
 			System.out.println("Divided by Zero");
 		}
 		
-		return result;
+		return solution;
 	}
 
 }
